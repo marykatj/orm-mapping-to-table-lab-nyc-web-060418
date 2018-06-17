a31dac8cfc0e9715d747587f38997f46f8c6a35f
Mary-Kate James <marykatj@gmail.com>
2018-06-17 20:43:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,13 +17,13 @@ def self.all
 end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-def self.create_table 
-  sql = <<- SQL 
-  CREATE TABLE students (
-  id INTEGER PRIMARY KEY, 
-  name TEXT, 
-  grade INTEGER
-  )
+def self.create_table
+  sql = <<- SQL
+    CREATE TABLE students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    );
   SQL
 end
 
